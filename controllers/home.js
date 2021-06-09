5dbdef31f38065dd89d44f427eb374ce5cfeffbd
Claire <cosmossss64@gmail.com>
2021-06-09 10:37:30 -0700

    master

---
+++ controllers/home.js
@@ -0,0 +1,20 @@
+//By Gary Chen
+
+//Get MongoDB Schema
+const Room = require('../models/Room')
+
+//GET request for reaching '/' home page
+async function getHome(request, response){
+
+  //Get all the rooms from the database
+  const rooms = await Room.find({}).lean()
+
+  //Render the home page and passing all the rooms
+  response.render('home',{
+    rooms: rooms,
+  })
+}
+
+module.exports = {
+    getHome,
+}
